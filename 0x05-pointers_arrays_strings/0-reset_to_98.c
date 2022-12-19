2b1a5fed768cdafb694c96212593bf8f017a869f
Betham <bmuthomi454@gmail.com>
2022-12-19 14:14:42 -0800

    Return the length of a string

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,9 +2,11 @@
 #include <stdio.h>
 
 /**
- *reset_to_98 - Pointer that updates the value it points to to 98
+ *reset_to_98 - a function to update the reference of a pointer
+ *@n: pointer to n to be updated
  */
 
 void reset_to_98(int *n)
 {
+	*n = 98;
 }
