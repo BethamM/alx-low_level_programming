6a8375ce3f21d2e3653f9817abdab4686c402c87
Betham <bmuthomi454@gmail.com>
2023-01-04 07:28:25 -0800

    Answer task 0

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * File: main.h
+ * 
+ * description: Heder file containing prototypes for all functions used in 0X07
+ */
+
+#include <stddef.h>
+
+int _putchar(char c);
+void *_memeset(void *s, int c, size_t n);
+
+#endif
+
