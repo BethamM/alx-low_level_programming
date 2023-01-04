f016c598a5b4aec1fa6ec270d92f71a540ce6bda
Betham <bmuthomi454@gmail.com>
2023-01-04 13:14:13 -0800

    Answer

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -16,7 +16,7 @@ void print_to_98(int n)
 		for (i = n; i >= 98; i--)
 		{
 			_putchar('0' + (i / 10));
-			_putchar('0' + (i / 10));
+			_putchar('0' + (i % 10));
 			if (i != 98)
 			{
 				_putchar(',');
@@ -28,10 +28,14 @@ void print_to_98(int n)
 	{
 		for (i = n; i <= 98; i++)
 		{
-			_putchar(',');
-			_putchar(' ');
+			_putchar('0' + (i / 10));
+			_putchar('0' + (i % 10));
+			if (i != 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
 		}
 	}
-}
-_putchar('\n);
+	_putchar('\n);
 }
