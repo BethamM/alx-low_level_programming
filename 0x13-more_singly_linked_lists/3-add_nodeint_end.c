e12e4a33229f075f0933e8d2f9c9e400793decbf
Betham <bmuthomi454@gmail.com>
2023-02-02 18:30:44 -0800

    Answer task 0 to 11

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -21,7 +21,10 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	if (new == NULL)
 		return (NULL);
 
-	new->n = n;							new->next = NULL;						temp = *head;							if (*head == NULL)
+	new->n = n;
+	new->next = NULL;
+	temp = *head;
+	if (*head == NULL)
 	{
 		*head = new;
 	}
