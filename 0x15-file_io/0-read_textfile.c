be0225bae87fce8f2a7c7596fdd5487ebfb2ba98
Betham <bmuthomi454@gmail.com>
2023-02-12 18:36:17 -0800

    Fix checker errors

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -12,7 +12,8 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t nrd, nwr;
 	char *buf;
 
-	if (!filename)								return (0);
+	if (!filename)
+		return (0);
 
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
