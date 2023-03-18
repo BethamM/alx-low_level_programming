e3aabb9e86d017deaf6e1805d9340acfd140cd96
Betham <bmuthomi454@gmail.com>
2023-03-17 19:39:20 -0700

    Change not staged

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,17 +8,20 @@
 
 int main(void)
 {
-	int a;
+	int i;
+	int j;
 
-	for (a = 0; a < 100; a++)
+	for (i = 0; i <= 9; i++)
 	{
-		putchar();
-		if (a < 99)
+		for (j = 0; j <= 9; j++)
 		{
+			putchar(i + '0');
+			putchar(j + '0');
 			putchar(',');
-			putchar(' ');
+			putchar(',');
+			putchar('\n');
 		}
 	}
-	putchar('\n');
+
 	return (0);
 }
